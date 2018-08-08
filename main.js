124ec3260e93ac7e17bba5f16e421ccdca8b030c
Diyaa <dhariri31412@gmail.com>
2018-08-08 17:31:02 +0300

    create web site for MTFX using HTML5 CSS3 Bootstrap Javascript & jQuery

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("#test2").click(function() {
+        $(" .custom-select").css("visibility", "visible");
+        console.log("HHh");
+    });
+});
+
+
+
+
+
+
+
+$(document).ready(function() {
+    $(".btn-show-represntative").click(function() {
+        $(".add-represntative").css("visibility", "visible");
+        console.log("hhhh");
+    });
+
+});
